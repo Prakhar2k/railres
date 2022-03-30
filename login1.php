55a895e2291d733fd72496f8c300cb3f0f89885b
Prakhar <prakhargupta2k@gmail.com>
2022-03-30 09:24:42 +0530

    third commit

--- login1.php
+++ login1.php
@@ -119,12 +119,7 @@ session_start();
 		<!-- Footer -->
 <footer >
 		<div style="width:100%;">
-			<div style="float:left;">
-			<p class="text-right text-info">  &copy; 2018 Copyright PVT Ltd.</p>	
-			</div>
-			<div style="float:right;">
-			<p class="text-right text-info">	Desinged By : projectworlds</p>
-			</div>
+			
 		</div>
 		</footer>		
 	</div>
